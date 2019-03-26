29373469610b800d75c3ab5555347fb8b1a7474e
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-03-27 00:27:34 +0530

    Initial commit

---
+++ Assets/main.js
@@ -0,0 +1,41 @@
+// Navbar
+
+$(function () {
+    $(document).scroll(function() {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+
+// Preloader
+
+var loader = document.getElementById('preloader');
+
+function myFunction(){
+    loader.style.display = "none";
+}
+
+
+// Stats Counter
+
+$('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+        duration: 3000,
+        easing:'linear',
+        step: function() {
+            $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+            $this.text(this.countNum);
+        }
+    });  
+});
