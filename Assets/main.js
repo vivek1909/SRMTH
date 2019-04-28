61c47d7a91df4f07b76b17eee0d667ec651d55e7
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-04-28 15:49:22 +0530

    .

--- Assets/main.js
+++ Assets/main.js
@@ -38,3 +38,19 @@ $('.counting').each(function() {
         }
     });  
 });
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 200) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
