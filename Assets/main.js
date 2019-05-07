1b393240d8d718bd91e53a58bd155428af1d2987
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-05-07 22:05:59 +0530

    .

--- Assets/main.js
+++ Assets/main.js
@@ -1,10 +1,10 @@
 // Navbar
 
 $(function () {
-    $(document).scroll(function() {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
+  $(document).scroll(function() {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
 });
 
 
@@ -14,30 +14,30 @@ $(function () {
 var loader = document.getElementById('preloader');
 
 function myFunction(){
-    loader.style.display = "none";
+  loader.style.display = "none";
 }
 
 
 // Stats Counter
 
 $('.counting').each(function() {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
+  var $this = $(this),
+    countTo = $this.attr('data-count');
     
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
   
-    {
-        duration: 10000,
-        easing:'linear',
-        step: function() {
-            $this.text(Math.floor(this.countNum));
-        },
-        complete: function() {
-            $this.text(this.countNum);
-        }
-    });  
+  {
+    duration: 10000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+          $this.text(this.countNum);
+      }
+  });  
 });
 
 
