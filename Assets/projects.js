016601d18ab6b28d555a6fe0574d24c962c32b76
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-05-29 19:36:17 +0530

    .

--- Assets/projects.js
+++ Assets/projects.js
@@ -6,3 +6,22 @@ function myFunction(){
   loader.style.display = "none";
 }
 
+
+
+
+//  Back to Top Button   
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
